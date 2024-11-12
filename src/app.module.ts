4a0204f03bae6d789f8118d9ec4f90ab736f1cfc
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2024-11-12 17:13:39 -0400

    setup .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { SendEmailController } from './controllers/app.controller';
 import { SendTestEmailService } from './services/send-test-email.service';
 import { IndexController } from './controllers/index.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [IndexController, SendEmailController],
   providers: [SendTestEmailService],
 })
